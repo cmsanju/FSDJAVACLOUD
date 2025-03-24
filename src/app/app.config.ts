ff294909453d8b1ffd22644e47e1ec0bf4ed7b64
cmsanju <lict.sanju@gmail.com>
2025-03-24 21:58:10 +0530

    withFetch

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
     provideClientHydration(withEventReplay()),
-    provideHttpClient()]
+    provideHttpClient(withFetch())]
 };
